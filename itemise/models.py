e8ac55892d391a102013814ac506c7d004f137e8
dbpython <33459750+papismurf@users.noreply.github.com>
2023-02-02 00:20:47 +0000

    create Bill (collection point for item objects) and Item (which are summed in the bill model to get total cost) models for the database content objects

--- itemise/models.py
+++ itemise/models.py
@@ -1,3 +1,25 @@
+from decimal import Decimal
+
 from django.db import models
 
-# Create your models here.
+
+class Bill(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Bill(id={self.id}"
+
+    def total(self) -> Decimal:
+        return sum(item.cost for item in self.item_set.all())
+
+
+class Item(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    description = models.TextField()
+    cost = models.DecimalField(max_digits=7, decimal_places=2)
+    bill = models.ForeignKey(Bill, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Item(id={self.id}, description={self.description}, " \
+            f"cost={self.cost})"
