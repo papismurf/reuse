b0dc8566173178f484d911154bfed2f95cb7a848
dbpython <33459750+papismurf@users.noreply.github.com>
2023-02-02 00:44:39 +0000

    Create a view to display all items in the database
    Create a view to display all items in the database and register the models to view in Django Admin.

--- itemise/admin.py
+++ itemise/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from itemise.models import Bill, Item
+
+
+# Make your models available in the Django Admin interface here
+
+@admin.register(Bill)
+class BillAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
--- itemise/views.py
+++ itemise/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from itemise.models import Bill
+
+
+def bill_json(request):
+    """
+    :param request:
+    :return: Returns all the Bill onjects and their Item objects in the database
+    """
+    results = {
+        "bills": [],
+    }
+
+    for bill in Bill.objects.all():
+        line = [str(bill), []]
+        for item in bill.item_set.all():
+            line[1].append(str(item))
+
+        results["bills"].append(line)
+
+    return JsonResponse(results)
